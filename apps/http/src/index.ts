e4bf3c2b827e7c423b0636dd18659b22301e7ea5
harsh07kashyap <kashyap7.harsh@gmail.com>
2025-07-11 14:36:42 +0530

    backend fixes

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import { router } from "./routes/v1/index";
-import { spaceRouter } from "./routes/v1/space";
-import { userRouter } from "./routes/v1/user";
+import dotenv from "dotenv";
+
 import client from "@repo/db/client"
 import cors from "cors"
+dotenv.config();
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cors({
-    origin: ['http://localhost:5173'], // Allow only this origin to access the server
+    origin: process.env.FRONTEND_URL,
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS', // Allowed HTTP methods
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, auth-token', // Allowed headers
     credentials: true // If you need to send cookies or HTTP authentication
@@ -17,5 +19,5 @@ app.use(cors({
 app.use("/api/v1",router);
 
 app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+    console.log(`Server is running on ${process.env.FRONTEND_URL}` );
 });
